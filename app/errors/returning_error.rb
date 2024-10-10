4f62bb182675c75e541e3ef9550026bbc82eb170
Adrian Jobda <adi.jobda@gmail.com>
2024-10-10 09:25:02 +0200

    implement ReturnDeviceFromUser service

---
+++ app/errors/returning_error.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ReturningError
+  class NotAssignedToUser < StandardError; end
+end
--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -1,11 +1,24 @@
 # frozen_string_literal: true
 
 class ReturnDeviceFromUser
-  def initialize
-    # TODO
+  def initialize(user:, serial_number:, from_user:)
+    @user= user
+    @serial_number = serial_number
+    @from_user = from_user
   end
 
   def call
-    # TODO
+    if @from_user != @user.id
+      raise RegistrationError::Unauthorized, 'Unauthorized'
+    end
+
+    device = Device.find_by(serial_number: @serial_number)
+    raise RegistrationError::DeviceNotFound, 'Device not found' unless device
+
+    if device.owner_id == nil && @from_user != device.owner_id
+      raise ReturningError::NotAssignedToUser, 'Device is not assigned to You'
+    end
+
+    device.update!(owner_id: nil)
   end
 end
